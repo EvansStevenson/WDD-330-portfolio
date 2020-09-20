142ac619791b1486679b304dd586ee6b0eb70377
masterlink0 <stevensonevans@gmail.com>
2020-09-19 18:58:24 -0600

    added all week files and index file for my portfolio with accompanying js and css files

---
+++ main.js
@@ -0,0 +1,24 @@
+window.onload = () =>{
+    loadLinks(createArray()); ;
+}
+function createArray(){ //didnt want a huge array to be declared when it was mostly the same material 
+    var links = [];     // So I just looped thorugh and pushed wach week with the small changes
+    for (let i = 1; i < 15; i++){
+        links.push({label: "Week" + i + " notes",
+        url: "week" + i + "/index.html"})
+    } 
+    return links;
+}
+function loadLinks(links){// create each li elemnt and add to a string
+    let filledList = '';
+    for (let link of links){
+        filledList += "<li>" + link.label + ': ' + "<a href='" + link.url + "'>" + link.url + "</a>" + "</li>";
+    }
+    //console.log(filledList);
+    document.getElementById('oderList').innerHTML = filledList;
+};
+
+
+
+    
+
